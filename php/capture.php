b4287a6ba33a80704ccfd0d3111648d4faa65a7e
George Pantazis <gpantazis@cpbgroup.com>
2012-02-19 12:02:35 -0700

    Save each image with a simple timestamp.

--- php/capture.php
+++ php/capture.php
@@ -1,10 +1,12 @@
 <?php
 
+	$imgPath = "images/" . time() . ".jpg";
+
 	//Use isightcapture bin to init the capture.
-	$command = 'sudo isightcapture images/test.jpg';
+	$command = 'sudo isightcapture ' . $imgPath;
 	exec($command." 2>&1");
 	
 	// echo out a string once capture is complete.
-	echo 123;
+	echo "<img src='" . $imgPath . "' />";
 
 ?>
