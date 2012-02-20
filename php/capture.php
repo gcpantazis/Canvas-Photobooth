5638d87b5e05054c6c156a70039db2f9b76b911b
George Pantazis <gpantazis@cpbgroup.com>
2012-02-19 21:50:05 -0700

    Use PNG to save files, results are higher-quality. Create an un-versioned config file to temporarily supply script creds.

--- php/capture.php
+++ php/capture.php
@@ -1,10 +1,13 @@
 <?php
 
-	$imgPath = "images/" . time() . ".jpg";
+	include("config.php");
 
-	//Use isightcapture bin to init the capture.
-	$command = 'sudo isightcapture ' . $imgPath;
-	exec($command." 2>&1");
+	$imgPath = "images/" . time() . ".png";
+
+	// Use isightcapture bin to init the capture.
+	// Use an un-versioned config file to temporarily supply un/pass.
+	$command = 'sudo -u ' . $username . ' -p ' . $password . ' isightcapture -t png ' . $imgPath;
+	exec($command." 2>&1", $test);
 	
 	// echo out a string once capture is complete.
 	echo "<img src='" . $imgPath . "' />";
