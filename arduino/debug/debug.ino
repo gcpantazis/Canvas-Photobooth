3396742bc4f6def713d82a08ade51e0ee6e609e4
George Pantazis <gcpantazis@gmail.com>
2012-03-27 00:17:13 -0600

    Finish python script for button interpretation / keypress

--- arduino/debug/debug.ino
+++ arduino/debug/debug.ino
@@ -1,12 +1,11 @@
 
 void setup() {                
   Serial.begin(9600);
-  pinMode(7, INPUT);     
   pinMode(12, OUTPUT);     
   digitalWrite(12, HIGH);
 }
 
 void loop() {
-  Serial.println(digitalRead(7));
-  delay(100);
+  Serial.println(analogRead(A0));
+  delay(30);
 }
--- python/arduino-connect.py
+++ python/arduino-connect.py
@@ -1,20 +1,48 @@
+
+# This script reads an analog value (0-1023) from the attached arduino via pyserial.
+# The value is matched to the corresponding button, which triggers the matching keystroke.
+
 import time
 import serial
 import os
 
-ser = serial.Serial('/dev/tty.usbmodemfd131', 9600)
+ser = serial.Serial('/dev/tty.usbmodemfa141', 9600)
+currentbtn = "BTNUP"
+lastbtn = currentbtn
 
 while 1:
 
-	try:  
-		print ser.readline()
+	inputval = ser.readline()
+	try:
+		inputval = float(ser.readline())
+	except:
+		inputval = inputval
+
+	try:
+		if inputval > 165 and inputval < 175:
+			currentbtn = "5"
+		elif inputval > 195 and inputval < 205:
+			currentbtn = "4"
+		elif inputval > 245 and inputval < 255:
+			currentbtn = "3"
+		elif inputval > 325 and inputval < 335:
+			currentbtn = "2"
+		elif inputval > 480 and inputval < 495:
+			currentbtn = "1"
+		else:
+			currentbtn = "BTNUP"
 	except:  
 		print "Failed to send!"  
 
+	if lastbtn != currentbtn:
+		lastbtn = currentbtn
+		if currentbtn != "BTNUP":
 
-	# cmd = """
-	# osascript -e 'tell application "System Events" to keystroke "m"' 
-	# """
-	# os.system(cmd)
-	time.sleep(0.1)
+			# If the current button is one of the desired values (1-5), execute applescript
+			# to trigger that keystroke.
+
+			cmd = '''osascript -e 'tell application "System Events" to keystroke "''' + currentbtn + '''"' '''
+			os.system(cmd)
+
+	time.sleep(0.03)
     
